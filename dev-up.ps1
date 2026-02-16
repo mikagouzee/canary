8fbab75b55072e67c7f293f27b031785f792cf29
NotRelevant <michel.gouzee@hotmail.com>
2026-02-16 10:20:59 +0100

    move yaml files to separate folder for cleaner readability

--- dev-up.ps1
+++ dev-up.ps1
@@ -1,5 +1,5 @@
 # Start the infrastructure
-kubectl apply -f postgres.yaml,rabbitmq.yaml,localstack.yaml,canary.yaml
+kubectl apply -f ./Manifests/postgres.yaml, ./Manifests/rabbitmq.yaml, ./Manifests/localstack.yaml, ./Manifests/canary.yaml
 
 # Open the tunnel in the background
 # This way localstack S3 endpoint is available at localhost:4566 so the dashboard is fully functional
